431870f99b9910c59f1c219b8d7936e326e564ba
Matthew Yoon <myoon928@gmail.com>
2024-08-27 19:23:17 -0700

    create starting menu

---
+++ scripts/menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var v_box_container: VBoxContainer = $VBoxContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	v_box_container.get_node("startButton").grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
