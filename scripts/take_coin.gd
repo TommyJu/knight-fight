c2b5d1ea4f29d0a87a01478203ac3087fb0067be
Tommy Ju <Tommyju25@gmail.com>
2024-08-28 17:55:44 -0700

    Create death at -1 coins

--- scripts/take_coin.gd
+++ scripts/take_coin.gd
@@ -4,25 +4,31 @@ extends Area2D
 @onready var player_1 = get_node("/root/Game/Players/Player1")
 @onready var player_2 = get_node("/root/Game/Players/Player2")
 @onready var game_manager = get_node("/root/Game/GameManager")
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
+@onready var timer: Timer = $Timer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	if body == player_1:
 		game_manager.remove_point_player_1()
 		game_manager.add_point_player_2()
+		# If the player dies
+		if game_manager.score_player_1 < 0:
+			animation_player.play("death_sound")
+			Engine.time_scale = 0.5
+			body.get_node("CollisionShape2D").queue_free()
+			body.get_node("AnimatedSprite2D").flip_v = true
+			timer.start()
 	elif body == player_2:
 		game_manager.remove_point_player_2()
 		game_manager.add_point_player_1()
+		# If the player dies
+		if game_manager.score_player_2 < 0:
+			animation_player.play("death_sound")
+			Engine.time_scale = 0.3
+			body.get_node("CollisionShape2D").queue_free()
+			body.get_node("AnimatedSprite2D").flip_v = true
+			timer.start()
 
-	
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
