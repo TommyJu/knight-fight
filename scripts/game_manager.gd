4f4aec9b4b33723df966b2c978af776100dab057
Matthew Yoon <myoon928@gmail.com>
2024-08-28 20:37:30 -0700

    Debug winsequence

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -13,19 +13,21 @@ func _ready() -> void:
 	# Adjust the text for rounds
 	player_1_round.text = "Player 1: " + str(RoundCount.round_player_1)
 	player_2_round.text = "Player 2: " + str(RoundCount.round_player_2)
+	Engine.time_scale = 1
 
-func check_for_winner() -> void:
-	if RoundCount.round_player_1 >= 3 || RoundCount.round_player_2 >=3:
-		get_tree().change_scene_to_file("res://scenes/win_screen.tscn")
+#func check_for_winner() -> void:
+	#if RoundCount.round_player_1 >= 3 || RoundCount.round_player_2 >=3:
+		#get_tree().change_scene_to_file("res://scenes/win_screen.tscn")
 
 func add_round_player_1():
 	RoundCount.incrment_round_player_1()
 	player_1_round.text = "Player 1: " + str(RoundCount.round_player_1)
-	check_for_winner()
+	#check_for_winner()
+	
 func add_round_player_2():
 	RoundCount.set("round_player_2", RoundCount.round_player_2 + 1)
 	player_2_round.text = "Player 2: " + str(RoundCount.round_player_2)
-	check_for_winner()
+	#check_for_winner()
 
 func add_point_player_1():
 	score_player_1 += 1
